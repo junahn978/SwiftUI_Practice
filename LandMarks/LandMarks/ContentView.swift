d45cbb9253e93abf24b3cd9c963c36c6001accc0
Jun Ahn <ahnh@ahnh-hqlm1-2.local>
2025-01-19 21:56:25 -0800

    swift UI Tutorials

---
+++ LandMarks/LandMarks/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  LandMarks
+//
+//  Created by Jun Ahn on 1/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+                
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title2)
+                    .fontWeight(.regular)
+                    .foregroundColor(.pink)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+                
+            }
+            .padding()
+            Spacer()
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LandMarks/LandMarks/LandMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandMarksApp.swift
+//  LandMarks
+//
+//  Created by Jun Ahn on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct LandMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
